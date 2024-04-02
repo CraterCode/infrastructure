c5f15950fac2c8045ed36461fc5d15d90de4a8e2
irishcarbomb777 <danielclannan@gmail.com>
2024-04-02 07:51:36 -0400

    ci(release-config): add release configuration

---
+++ index.ts
@@ -0,0 +1,15 @@
+import * as awsx from "@pulumi/awsx";
+
+// Create a new VPC
+const eksVpc = new awsx.ec2.Vpc("eks-vpc", {
+    enableDnsHostnames: true,   // This causes the VPC to assign DNS hostnames to instances which are launched into the VPC. It essentially adds your instances to the AWS DNS Server.
+    cidrBlock: "10.0.0.0/16",
+    tags: {
+        Organization: "Crater"
+    }
+});
+
+// Export some values for use elsewhere
+export const vpcId = eksVpc.vpcId;
+export const privateSubnetIds = eksVpc.privateSubnetIds;
+export const publicSubnetIds = eksVpc.publicSubnetIds;
